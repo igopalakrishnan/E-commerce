f08b1d63f0125bfd74a8bbcb76f8e65ddea3efe7
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-05 17:19:43 +0530

    create New Page

--- admin-app/src/containers/NewPage/index.js
+++ admin-app/src/containers/NewPage/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import Modal from '../../components/UI/Modal';
 import Input from '../../components/UI/Input';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import linearCategories from '../../helpers/linearCategories';
+import { useSelector } from 'react-redux';
 
 /**
 * @author
@@ -11,36 +13,112 @@ import { Row, Col } from 'react-bootstrap';
 
 const NewPage = (props) => {
 
-    const [createModal, setcreateModal] = useState(false);
+    const [createModal, setCreateModal] = useState(false);
     const [title, setTitle] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [categoryId, setCategoryId] = useState('');
+    const [desc, setDesc] = useState('');
+    const [banners, setBanners] = useState([]);
+    const [products, setProducts] = useState([]);
+    const category = useSelector(state => state.category);
+
+
+    useEffect(() => {
+        setCategories(linearCategories(category.categories));
+    }, [category]);
+
+    const handleBannerImages = (e) => {
+        console.log(e);
+    }
+
+    const handleProductImages = (e) => {
+        console.log(e);
+    }
+
 
     const renderCreatePageModal = () => {
         return (
             <Modal
-            show={createModal}
-            modalTitle={'Create New Page'}
-            handleClose={() => setcreateModal(false)}
+                show={createModal}
+                modalTitle={'Create New Page'}
+                handleClose={() => setCreateModal(false)}
             >
 
-                <Row>
-                    <Col>
-                    <Input 
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder={'Page Title'}
-                    />
-                    </Col>
-                </Row>
-               
+                <Container>
+                    <Row>
+                        <Col>
+                            <select
+                                className='form-control form-control-sm'
+                                value={categoryId}
+                                onChange={(e) => setCategoryId(e.target.value)}
+                            >
+                                <option value=''>Select Category</option>
+                                {
+                                    categories.map((cat) =>
+                                        <option key={cat._id} value={cat._id}>{cat.name}</option>)
+                                }
+
+                            </select>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <Input
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                placeholder={'Page Title'}
+                                className='form-control-sm'
+                            />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <Input
+                                value={desc}
+                                onChange={(e) => setDesc(e.target.value)}
+                                placeholder={'Page Desc'}
+                                className='form-control-sm'
+                            />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <input
+                                className='form-control'
+                                type='file'
+                                name='banners'
+                                onChange={handleBannerImages}
+                            />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <input
+                                className='form-control'
+                                type='file'
+                                name='products'
+                                onChange={handleProductImages}
+                            />
+                        </Col>
+                    </Row>
+
+
+                </Container>
+
             </Modal>
         );
     }
-  return(
-    <Layout sidebar>
-        page
-    </Layout>
-   )
+    return (
+        <Layout sidebar>
+            {renderCreatePageModal()}
+            <button onClick={() => setCreateModal(true)}>Create Page</button>
+        </Layout>
+    )
 
- }
+}
 
-export default NewPage
+export default NewPage;
