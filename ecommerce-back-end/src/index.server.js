abace0332f79aedee03a8bdf71eb6076d0445ca6
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-05 18:36:40 +0530

    create New Page

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -12,6 +12,7 @@ const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
 const initialDataRoutes = require('./routes/admin/initialData');
+const pageRoutes = require('./routes/admin/page');
 
 
 //environmental variable
@@ -40,6 +41,7 @@ app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
 app.use('/api', initialDataRoutes);
+app.use('/api', pageRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`This server is running on ${process.env.PORT}`);
