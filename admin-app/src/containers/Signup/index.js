74e637dade13edf32cb28cc984aa8ac1547c08e3
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-24 18:25:57 +0530

    update signup page

--- admin-app/src/containers/Signup/index.js
+++ admin-app/src/containers/Signup/index.js
@@ -1,53 +1,63 @@
-import React, { useState } from 'react';
-import Layout from '../../components/Layout';
-import { Container, Form, Button, Row, Col } from 'react-bootstrap';
-import Input from '../../components/UI/Input';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { signup } from '../../actions';
-
+import React, { useState } from "react";
+import Layout from "../../components/Layout";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import Input from "../../components/UI/Input";
+import { Redirect } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { signup } from "../../actions";
+import { useEffect } from "react";
 
 /**
-* @author
-* @function Signup
-**/
+ * @author
+ * @function Signup
+ **/
 
 const Signup = (props) => {
-
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const auth = useSelector(state => state.auth);
-    const user = useSelector(state => state.user);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const auth = useSelector((state) => state.auth);
+    const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
-    const userSignup = (e) => {
+    useEffect(() => {
+        if (!user.loading) {
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setPassword("");
+        }
+    }, [user.loading]);
 
+    const userSignup = (e) => {
         e.preventDefault();
 
         const user = {
-            firstName, lastName, email, password
-        }
+            firstName,
+            lastName,
+            email,
+            password,
+        };
+
         dispatch(signup(user));
-    }
+    };
 
     if (auth.authenticate) {
-        return <Redirect to={'/'} />
+        return <Redirect to={`/`} />;
     }
 
-    if(user.loading) {
-        return <p>Loading....!</p>
+    if (user.loading) {
+        return <p>Loading...!</p>;
     }
 
     return (
         <Layout>
             <Container>
                 {user.message}
-                <Row style={{ marginTop: '50px' }}>
+                <Row style={{ marginTop: "50px" }}>
                     <Col md={{ span: 6, offset: 3 }}>
-
                         <Form onSubmit={userSignup}>
                             <Row>
                                 <Col md={6}>
@@ -69,25 +79,22 @@ const Signup = (props) => {
                                     />
                                 </Col>
                             </Row>
-                            <Col>
-                                <Input
-                                    label="Email"
-                                    placeholder="Email"
-                                    value={email}
-                                    type="email"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Col>
 
-                            <Col>
-                                <Input
-                                    label="Password"
-                                    placeholder="Password"
-                                    value={password}
-                                    type="password"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </Col>
+                            <Input
+                                label="Email"
+                                placeholder="Email"
+                                value={email}
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                            <Input
+                                label="Password"
+                                placeholder="Password"
+                                value={password}
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
@@ -95,10 +102,8 @@ const Signup = (props) => {
                     </Col>
                 </Row>
             </Container>
-
         </Layout>
-    )
-
-}
+    );
+};
 
 export default Signup;
